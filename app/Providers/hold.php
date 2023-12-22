a780a9157c90a4efbc37a3dbdeeb881f015099fd
anhtuan12011998 <anhtuan140212@gmail.com>
2023-12-22 19:58:55 +0700

    add hold

---
+++ app/Providers/hold.php
@@ -0,0 +1,14 @@
+class Car {
+  public $color;
+  public $model;
+  public function __construct($color, $model) {
+    $this->color = $color;
+    $this->model = $model;
+  }
+}
+
+$myCar = new Car("red", "Volvo");
+
+foreach ($myCar as $x => $y) {
+  echo "$x: $y <br>";
+}
